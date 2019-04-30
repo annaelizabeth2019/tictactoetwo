2da25bf0e767780d9abac1607f61bdfa89de8f2e
annaelizabeth2019 <annapeterson89@gmail.com>
2019-04-30 15:17:52 -0500

    daily commit

--- js/main.js
+++ js/main.js
@@ -2,20 +2,35 @@
 
 let board;
 let turn = 'X';
+let win;
+let tie;
 
 /*----- cached element references -----*/
 
 const squares = Array.from(document.querySelectorAll('#board div'));
 
 /*----- event listeners -----*/
+document.getElementById('board').addEventListener('click', handleTurn);
+const messages = document.querySelector('h2');
 
 
 /*----- functions -----*/
 
+function handleTurn() {
+    let idx = squares.findIndex(function(square) {
+        return square === event.target;
+    });
+    board[idx] = turn;
+
+    win = board[0] && board[0] === board[1] && board[0] === board[2] ? board[0] : null;
+    turn = turn === 'X' ? 'O' : 'X';
+    render();
+};
+
 function init() {
     board = [
     '', '', '',
-    '', 'X', '',
+    '', '', '',
     '', '', ''
     ];
     render();
@@ -26,6 +41,7 @@ function render() {
     //this moves the value of the board item into the squares[idx]
     squares[index].textContent = mark;
     });
+    messages.textContent = `It's ${turn}'s turn!`;
     };
 
 init();
