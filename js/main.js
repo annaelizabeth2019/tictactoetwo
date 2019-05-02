a8418ba85db37fe8e4f0e7bdaea1bf2a513e327d
annaelizabeth2019 <annapeterson89@gmail.com>
2019-05-02 12:30:52 -0500

    a minor tweak to the message

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ function render() {
     //this moves the value of the board item into the squares[idx]
     squares[index].textContent = mark;
     });
-    messages.textContent = win === 'T' ? 'Rats! A tie!' : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+    messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
     };
 
 init();
